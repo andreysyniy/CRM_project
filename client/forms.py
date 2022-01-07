744f6a3062147b89b703b1e29583189a5994bf1b
Andres S <andreysyniy@gmail.com>
2022-01-07 22:02:12 +0200

    Functionality CreateClient and UpdateClient

--- client/forms.py
+++ client/forms.py
@@ -1,52 +1,12 @@
 from django import forms
 from .models import *
 from tinymce.widgets import TinyMCE
-from django.http import request
+from django.forms.models import inlineformset_factory
 
 
-class ClientCreateForm(forms.Form):
-  company_name = forms.CharField(label='Название Компании', widget=forms.TextInput())
-  full_name_director = forms.CharField(label='Ф.И.О.', widget=forms.TextInput())
-  short_description = forms.CharField(label='Краткое Описание' ,widget=TinyMCE(attrs={'cols': 50, 'rows': 20}))
-  address = forms.CharField(label='Адрес', widget=forms.TextInput())
-  # class Meta:
-  #   model = Client
-  #   fields = '__all__'  
-  
-
-# class ClientUpdateForm(forms.Form):
-#   company_name = forms.CharField(label='Название Компании', widget=forms.TextInput())
-#   full_name_director = forms.CharField(label='Ф.И.О.', widget=forms.TextInput())
-#   short_description = forms.CharField(label='Краткое Описание' ,widget=TinyMCE(attrs={'cols': 50, 'rows': 20}))
-#   address = forms.CharField(label='Адрес', widget=forms.TextInput())
-#   phone = forms.CharField(label='Телефон', widget=forms.TextInput())
-#   email = forms.EmailField(label='Электронная почта', widget=forms.TextInput())
-  
 
 
-# class ClientUpdateForm(forms.ModelForm):
-#   class Meta:
-#     model = Client
-#     fields = ['company_name', 'full_name_director', 'short_description', 'address']
+PhoneFormset = inlineformset_factory(Client, Phone, fields=('phone',), can_delete=True, extra=1)
 
-#     # widgets = {
-#     #   'company_name': forms.TextInput(attrs={'class': 'form-control'}),
-#     #   'full_name_director': forms.TextInput(attrs={'class': 'form-control'}),
-#     #   'short_description': forms.TextInput(attrs={'class': 'form-control'}),
-#     # }
-
-#     class Media:
-#         js = ('/site_media/static/tiny_mce/tinymce.min.js',)
-
-# class ClientPhoneUpdateForm(forms.ModelForm):
-#   class Meta:
-#     model = Phone
-#     fields = ['phone']
-
-# class ClientEmailUpdateForm(forms.ModelForm):
-#   class Meta:
-#     model = Email
-#     fields = ['email']
-
-
+EmailFormset = inlineformset_factory(Client, Email, fields=('email',), can_delete=True, extra=1)
 
--- client/views.py
+++ client/views.py
@@ -1,6 +1,6 @@
 from django.urls.base import reverse_lazy
-from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView
-from client.forms import ClientCreateForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import PhoneFormset, EmailFormset
 from .models import *
 from django.views.generic import ListView, DetailView
 
@@ -25,90 +25,79 @@ class ClientDetail(DetailView):
   context_object_name = 'client_detail'
 
 
-class ClientCreate(FormView):
+class ClientCreate(CreateView):
   '''Форма редактирования клиентской информации'''
-  form_class = ClientCreateForm
+  model = Client
+  fields = '__all__'
   template_name = 'client/client_create.html'
   success_url = reverse_lazy('home')
 
   def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context['title'] = 'Создание информации о клиенте'
-      return context
+    context = super().get_context_data(**kwargs)
+    context['title'] = 'Создание информации о клиенте'
+    if self.request.POST:
+        context['phone_formset'] = PhoneFormset(self.request.POST, instance=self.object)
+        context['phone_formset'].full_clean()
+        
+        context['email_formset'] = EmailFormset(self.request.POST, instance=self.object)
+        context['email_formset'].full_clean()
+    else:
+        context['phone_formset'] = PhoneFormset(instance=self.object)
+        context['email_formset'] = EmailFormset(instance=self.object)
+    return context
 
+  def form_valid(self, form):
+    context = self.get_context_data(form=form)
+    phone_formset = context['phone_formset']
+    email_formset = context['email_formset']
+    if phone_formset.is_valid() and email_formset.is_valid():
+      response = super().form_valid(form)
+      phone_formset.instance = self.object
+      phone_formset.save()
+      email_formset.instance = self.object
+      email_formset.save()
+      return response
+    else:
+      return super().form_invalid(form)
 
-
-# class ClientUpdate(View):
-#   '''Форма редактирования клиентской информации'''
-#   def get(self, request, client_id):
-#     phone_list = dict()
-#     client = Client.objects.get(pk__iexact = client_id)
-#     phone = Phone.objects.filter(client_id = client_id)
-#     for i in range(len(phone)):
-#       print(i)
-#       phone_list['phone'] = phone[i].phone
-#     print(phone_list)
-#     bound_form = ClientUpdateForm(initial=model_to_dict(client) | phone_list)
-#     return render(request, 'client/client_update.html', context={'form': bound_form, 'client': client})
-    
-  # form_class = ClientUpdateForm
-  # # fields = ['company_name', 'full_name_director', 'short_description', 'address']
-  # template_name = 'client/client_update.html'
-  # # pk_url_kwarg = 'client_id'
-
-
-  # def get_context_data(self, **kwargs):
-  #     context = super().get_context_data(**kwargs)
-  #     context['phone_list'] = Phone.objects.filter(client_id = 1)
-  #     context['email_list'] = Email.objects.filter(client_id = 1)
-  #     context['title'] = 'Обновление информации о клиенте'
-  #     return context
-
-
-# def client_update_func(request, client_id):
-#   client = Client.objects.get(pk=client_id)
-#   # PhoneFormset = modelformset_factory(Phone, fields=('phone',))
-#   PhoneFormset = inlineformset_factory(Client, Phone, fields=('phone',), can_delete=True, extra=1)
-#   EmailFormset = inlineformset_factory(Client, Email, fields=('email',), can_delete=True, extra=1)
-
-#   if request.method == 'POST':
-#     # formset = PhoneFormset(request.POST, queryset=Phone.objects.filter(client__id=client.id))
-#     phone_formset = PhoneFormset(request.POST, instance=client)
-#     email_formset = EmailFormset(request.POST, instance=client)
-#     if phone_formset.is_valid() and email_formset.is_valid():
-#       phone_formset.save()
-#       email_formset.save()
-#       # instances = formset.save(commit=False)
-#       # for instance in instances:
-#       #   instance.client_id = client.id
-#       #   instance.save()
-      
-      
-#       return redirect('client_detail', client_id=client.id)
-
-#   # formset = PhoneFormset(queryset=Phone.objects.filter(client__id=client.id))
-
-#   phone_formset = PhoneFormset(instance=client)
-#   email_formset = EmailFormset(instance=client)
-
-  
-#   return render(request, 'client/client_update.html', {'phone_formset' : phone_formset, 'email_formset' : email_formset})
 
 
 
 class ClientUpdate(UpdateView):
   '''Форма редактирования клиентской информации'''  
-  fields = ['company_name', 'full_name_director', 'short_description', 'address']
   model = Client
+  fields = '__all__'
   template_name = 'client/client_update.html'
   pk_url_kwarg = 'client_id'
-  # context_object_name = 'client_info'
-  
+  success_url = reverse_lazy('home')
+
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
-    context['second_model'] = Phone.objects.filter(client__id=self.kwargs['client_id'])
+    context['title'] = 'Изменение информации о клиенте'
+    if self.request.POST:
+        context['phone_formset'] = PhoneFormset(self.request.POST, instance=self.object)
+        context['phone_formset'].full_clean()
+        
+        context['email_formset'] = EmailFormset(self.request.POST, instance=self.object)
+        context['email_formset'].full_clean()
+    else:
+        context['phone_formset'] = PhoneFormset(instance=self.object)
+        context['email_formset'] = EmailFormset(instance=self.object)
     return context
 
+  def form_valid(self, form):
+    context = self.get_context_data(form=form)
+    phone_formset = context['phone_formset']
+    email_formset = context['email_formset']
+    if phone_formset.is_valid() and email_formset.is_valid():
+      response = super().form_valid(form)
+      phone_formset.instance = self.object
+      phone_formset.save()
+      email_formset.instance = self.object
+      email_formset.save()
+      return response
+    else:
+      return super().form_invalid(form)
 
 
 
