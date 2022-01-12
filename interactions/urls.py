d082d9195b6c4860f2f61f45244e19c4cc9eaa5f
Andres S <andreysyniy@gmail.com>
2022-01-13 00:01:16 +0200

    Interaction

--- interactions/urls.py
+++ interactions/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('clients/interactions/', InteractionsForClients.as_view(), name='interactions_clients'),
-    path('projects/interactions/', InteractionsForProjects.as_view(), name='interactions_projects'),
-    path('client/<int:client_id>/interactions/', InteractionsForClient.as_view(), name='interactions_client'),
-    path('project/<int:project_id>/interactions/', InteractionsForProject.as_view(), name='interactions_project'),
+    path('interactions/', InteractionsList.as_view(), name='interactions_list'),
     path('interactions/detail/<int:interaction_id>/', InteractionDetail.as_view(), name='interaction_detail'),
     path('interaction/create/', InteractionCreate.as_view(), name = 'interaction_create'),
     path('interaction/update/<int:interaction_id>/', InteractionUpdate.as_view(), name = 'interaction_update'),
--- interactions/views.py
+++ interactions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
@@ -7,60 +8,56 @@ from client.models import Client
 from project.models import Project
 from .models import Interaction
 
-class InteractionsForClients(ListView):
+class InteractionsList(ListView):
   '''Отображение взаимодействий для всех клиентов'''
-  pass
-
-
-class InteractionsForProjects(ListView):
-  '''Отображение взаимодействий для всех проектов'''
-  pass
-
-
-class InteractionsForClient(DetailView):
-  '''Отображение взаимодействий для одного клиента'''
-  model = Client
+  model = Interaction
   paginate_by = 4
-  template_name = 'interactions/interactions_client.html'
-  pk_url_kwarg = 'client_id'
-  extra_context = {'title': 'Взаимодействия по клиенту'}
-  allow_empty = True
+  template_name = 'interactions/interactions_list.html'
+  extra_context = {'title': 'Взаимодействия'}
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
-    context['interactions_client_list'] = Interaction.objects.filter(client_id=self.kwargs['client_id']).order_by(self.request.GET.get('orderby', 'project'))
-    return context
-
-class InteractionsForProject(DetailView):
-  '''Отображение взаимодействий для одного проекта'''
-  model = Project
-  paginate_by = 4
-  template_name = 'interactions/interactions_project.html'
-  pk_url_kwarg = 'project_id'
-  extra_context = {'title': 'Взаимодействия по проекту'}
-  allow_empty = True
-
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['interactions_project_list'] = Interaction.objects.filter(project_id=self.kwargs['project_id']).order_by(self.request.GET.get('orderby', 'project'))
+    context['interactions_list'] = Interaction.objects.all().order_by(self.request.GET.get('orderby', 'project'))
     return context
 
 
 class InteractionDetail(DetailView):
   '''Отображение детальное отображение информации взаимодействия'''
-  pass
+  model = Interaction
+  fields = '__all__'
+  template_name = 'interactions/interaction_detail.html'
+  pk_url_kwarg = 'interaction_id'
+  context_object_name = 'interaction_detail'
+  # success_url = reverse_lazy('interactions_list')
+  extra_context = {'title': 'Информация о взаимодействии'}
 
 
 class InteractionCreate(CreateView):
   '''Создание нового взаимодействия'''
-  pass
+  model = Interaction
+  fields = '__all__'
+  template_name = 'interactions/interaction_create.html'
+  # pk_url_kwarg = 'interaction_id'
+  success_url = reverse_lazy('interactions_list')
+  extra_context = {'title': 'Создание взаимодействия'}
 
 
 class InteractionUpdate(UpdateView):
   '''Редактирование взаимодействия'''
-  pass
+  model = Interaction
+  fields = '__all__'
+  template_name = 'interactions/interaction_update.html'
+  pk_url_kwarg = 'interaction_id'
+  success_url = reverse_lazy('interactions_list')
+  extra_context = {'title': 'Обновление взаимодействия'}
+
 
 
 class InteractionDelete(DeleteView):
   '''Удаление взаимодействия'''
-  pass
+  model = Interaction
+  fields = '__all__'
+  template_name = 'interactions/interaction_delete.html'
+  pk_url_kwarg = 'interaction_id'
+  success_url = reverse_lazy('interactions_list')
+  extra_context = {'title': 'Удаление взаимодействия'}
